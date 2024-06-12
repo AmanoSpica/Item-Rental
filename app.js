d4fcdb83e6476775cdd3caadbb07bdfda9de09fe
TaichiOikawa <zunda.taichi@gmail.com>
2024-06-12 18:57:12 +0900

    edit 20240612 at school

--- app.js
+++ app.js
@@ -9,28 +9,83 @@ app.use(express.urlencoded({
 app.use(express.static('public'));
 
 
+const items = [
+	{itemNum: 1, itemName: 'スパナ'},
+	{itemNum: 2, itemName: 'ハンマー'},
+];
+
+
+
 app.get('/', (req, res) => {
 	res.render('index.ejs');
 });
 
 app.get('/lent', (req, res) => {
 	res.render('lent.ejs',
-		{items:
-			[
-				{itemNum: 1, itemName: 'スパナ'},
-				{itemNum: 2, itemName: 'ハンマー'},
-			],
+		{items: items,
+		message: null
 		}
 	);
 });
 
 app.post('/lent', (req, res) => {
+	// SQL INSERT処理
 	console.log(req.body);
-	res.redirect('/lent');
+	const message = 4;
+	res.render('lent.ejs',
+		{items: items,
+		message: message
+		}
+	);
 });
 
-app.get('/return', (req, res) => {
-	res.render('return.ejs');
+
+app.get('/list', (req, res) => {
+
+	const data = [
+		{
+			dataNum: 1,
+			itemNum: 1,
+			itemPiece: 2,
+			className: '2',
+			lentTime: '2021-08-01 10:00'
+		},
+		{
+			dataNum: 2,
+			itemNum: 2,
+			itemPiece: 1,
+			className: '3',
+			lentTime: '2021-08-02 10:00'
+		}
+	];
+	// dataのitemNumを元にitemNameをオブジェクトに追加
+	for (let i = 0; i < data.length; i++) {
+		for (let j = 0; j < items.length; j++) {
+			if (data[i].itemNum === items[j].itemNum) {
+				data[i].itemName = items[j].itemName;
+			}
+		}
+	}
+	res.render('list.ejs',
+		{
+			data: data,
+			items: items,
+			message: null
+		}
+	)
+});
+
+app.post('/return/:id', (req, res) => {
+	// SQL UPDATE処理
+	console.log(req.params);
+	const data = [];
+	res.render('list.ejs',
+		{
+			data: data,
+			items: items,
+			message: null
+		}
+	)
 });
 
 
